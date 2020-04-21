50b83daeeb25a78931337eaa79f85e780d0a6cf5
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-04-21 17:11:27 +0200

    Add center adjuctement to image function

--- app.js
+++ app.js
@@ -15,12 +15,15 @@ const addTextWatermarkToImage = async function (inputFile, outputFile, text) {
 const addImageWatermarkToImage = async function (inputFile, outputFile, watermarkFile) {
   const image = await Jimp.read(inputFile);
   const watermark = await Jimp.read(watermarkFile);
+  const x = image.getWidth() / 2 - watermark.getWidth() / 2;
+  const y = image.getHeight() / 2 - watermark.getHeight() / 2;
 
-  image.composite(watermark, 0, 0, {
+  image.composite(watermark, x, y, {
     mode: Jimp.BLEND_SOURCE_OVER,
     opacitySource: 0.5,
   });
   await image.quality(100).writeAsync(outputFile);
 };
 
-addImageWatermarkToImage('./test.jpg', './test-with-watermark.jpg', 'twitter.png')
+addTextWatermarkToImage('./test.jpg', './test-with-watermark.jpg', 'hello World')
+addImageWatermarkToImage('./test.jpg', './test-with-watermark2.jpg', './twitter.png')
